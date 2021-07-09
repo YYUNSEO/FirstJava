175fb94af5195efcfcf8846a8381106a17bf1b80
robot <robot@208-pro4-4>
2021-07-09 11:19:04 +0900

    두번째 소스 올리기

--- FristGitJava/src/s20411_0709/GitJava.java
+++ FristGitJava/src/s20411_0709/GitJava.java
@@ -4,7 +4,9 @@ public class GitJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("로봇고 2학년 4반 11번 양윤서입니다.");
+		
+		System.out.println("수정합니다.");
 	}
 
 }
